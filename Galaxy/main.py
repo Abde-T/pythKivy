b80f9143a3f1394b263d3b8ae7be44423b33d87d
Abderrahmane <tiamani3939@gmail.com>
2023-11-15 09:58:09 +0100

    f

--- Galaxy/main.py
+++ Galaxy/main.py
@@ -1,14 +1,21 @@
+from kivy.config import Config
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, Clock
 from kivy.graphics.vertex_instructions import Line
 from kivy.graphics.context_instructions import Color
-
+from kivy.core.window import Window
+from kivy import platform
 
 class MainWidget(Widget):
+    from transforms import transform, transform_2D, transform_perspective
+    from userActions import on_keyboard_up, on_keyboard_down, on_touch_up, on_touch_down, keyboard_closed
+
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
-
 
     V_NB_LINES = 10
     V_L_SPACING = .25
@@ -21,32 +28,48 @@ class MainWidget(Widget):
     SPEED = 4
     current_offset_y = 0
 
+    SPEED_X = 12 
+    current_offset_x = 0
+    current_speed_x = 0
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print(str(self.width), str(self.height))
         self.init_vertical_lines()
         self.init_horizontal_lines()
+
+        if self.is_desktop():
+            self.keyboard = Window.request_keyboard(self.keyboard_closed, self)
+            self.keyboard.bind(on_key_down=self.on_keyboard_down)
+            self.keyboard.bind(on_key_up=self.on_keyboard_up)
+
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
-    def on_parent(self, widget, parent):
-        pass
 
-    def on_size(self, *args):
-        # print(str(self.width), str(self.height))
-        # self.perspective_point_x = self.width/2
-        # self.perspective_point_y = self.height*0.75
-        # self.update_vertical_lines()
-        # self.update_horizontal_lines()
-        pass
+    def is_desktop(self):
+        if platform in ('linux', 'win', 'macosx'):
+            return True
+        return False
+
+    # def on_parent(self, widget, parent):
+    #     pass
+
+    # def on_size(self, *args):
+    #     # print(str(self.width), str(self.height))
+    #     # self.perspective_point_x = self.width/2
+    #     # self.perspective_point_y = self.height*0.75
+    #     # self.update_vertical_lines()
+    #     # self.update_horizontal_lines()
+    #     pass
 
 
-    def on_perspective_point_x(self, widget, value):
-        # print("on_perspective_point_x", str(value))
-        pass
+    # def on_perspective_point_x(self, widget, value):
+    #     # print("on_perspective_point_x", str(value))
+    #     pass
     
-    def on_perspective_point_y(self, widget, value):
-        # print("on_perspective_point_y", str(value))
-        pass
+    # def on_perspective_point_y(self, widget, value):
+    #     # print("on_perspective_point_y", str(value))
+    #     pass
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -61,7 +84,7 @@ class MainWidget(Widget):
         spacing = self.V_L_SPACING * self.width
         offset = -int(self.V_NB_LINES/2)+0.5
         for i in range (0, self.V_NB_LINES):
-            line_x = int(central_line_x + offset * spacing)
+            line_x = central_line_x + offset * spacing +self.current_offset_x
 
             x1,y1= self.transform(line_x, 0)
             x2,y2= self.transform(line_x, self.height)
@@ -82,8 +105,8 @@ class MainWidget(Widget):
         spacing = self.V_L_SPACING * self.width
         offset = -int(self.V_NB_LINES/2)+0.5
 
-        x_min = central_line_x + offset * spacing
-        x_max = central_line_x - offset * spacing
+        x_min = central_line_x + offset * spacing + self.current_offset_x
+        x_max = central_line_x - offset * spacing + self.current_offset_x
         spacing_y = self.H_L_SPACING * self.height
 
         for i in range (0, self.H_NB_LINES):
@@ -93,40 +116,21 @@ class MainWidget(Widget):
             self.horizontal_Lines[i].points = [x1,y1, x2, y2]
 
 
-    def transform(self, x,y):
-       # return self.transform_2D(x,y)
-        return self.transform_perspective(x,y)
-    
-    def transform_2D(self, x,y):
-        return int(x), int(y)
-    
-    def transform_perspective(self, x,y):
-        linear_y = y * self.perspective_point_y / self.height
-        if linear_y > self.perspective_point_y:
-            linear_y = self.perspective_point_y
-
-        diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - linear_y
-        factor_y = diff_y / self.perspective_point_y
-        factor_y = pow(factor_y, 4)
-
-        tr_x = self.perspective_point_x + diff_x * factor_y
-        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
-        
-        return int(tr_x), int(tr_y)
-
-
     def update(self, dt):
+        time_factor = dt*60
         self.update_vertical_lines()
         self.update_horizontal_lines()
-        self.current_offset_y += self.SPEED
+        self.current_offset_y += self.SPEED * time_factor
 
         spacing_y = self.H_L_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+
+        self.current_offset_x += self.current_speed_x * time_factor
 
 
 class GalaxyApp(App):
     pass
 
 GalaxyApp().run()
+
